574d7cdd4b7834074330d184d39a2f4771e1921f
Joosep Angast <joosep.angastrlive.com>
2024-11-13 22:14:46 +0200

    Fix image address

--- js/randomimage.js
+++ js/randomimage.js
@@ -1,5 +1,5 @@
 const myButton = document.querySelector('button');
-const image = document.querySelector('img')
+const image = document.querySelector('img');
 const images = ["Image1.jpg", "Image2.jpg", "Image3.jpg", "Image4.jpg"];
 
 
@@ -7,5 +7,6 @@ myButton.addEventListener('click', changeImage);
 
 function changeImage() {
     let randomIndex = Math.floor(Math.random() * images.length);
-    image.src = "images/Image4.jpg"; images[randomIndex];
+    console.log(randomIndex);
+    image.src = "images/" + images[randomIndex];
 }
